7a5f424c622adc74f9cf319088ef9fa5b864758e
brenoborba <brenoborba99@gmail.com>
2020-11-11 15:49:15 +0000

    Alteração no exercício 10

--- Ficha 4/src/ex010.java
+++ Ficha 4/src/ex010.java
@@ -4,13 +4,14 @@ public class ex010 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Digite um caractér: ");
-        char ch = scan.next().charAt(0);
+        System.out.print("Digite um carácter: ");
+       char ch_default = scan.nextLine().charAt(0);
 
-        
+       char ch_mai = Character.toUpperCase(ch_default);
+       char ch_min  = Character.toLowerCase(ch_default);
 
-
-
+        System.out.println("O caracter digitado em sua forma maiúscula: " + ch_mai);
+        System.out.println("O carácter inserido em sua forma minúscula: " + ch_min  );
 
     }
 }
--- Ficha 4/src/ex011.java
+++
@@ -1,5 +0,0 @@
-public class ex011 {
-    public static void main(String[] args){
-
-    }
-}
